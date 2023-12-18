7a49a10b5c25dd77d1e6df99b151510095abd2b0
HOME-PC\Insane <valeev.timur.v@gmail.com>
2023-12-18 13:42:56 +0300

    Add Spring Doc MVC and document API

--- src/main/java/com/dom/tasks/config/ApplicationConfig.java
+++ src/main/java/com/dom/tasks/config/ApplicationConfig.java
@@ -2,6 +2,11 @@ package com.dom.tasks.config;
 
 import com.dom.tasks.web.security.JwtTokenFilter;
 import com.dom.tasks.web.security.JwtTokenProvider;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import org.springframework.context.ApplicationContext;
@@ -9,6 +14,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.expression.method.DefaultMethodSecurityExpressionHandler;
+import org.springframework.security.access.expression.method.MethodSecurityExpressionHandler;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -37,6 +44,27 @@ public class ApplicationConfig {
     @Bean
     public AuthenticationManager authenticationManager(final AuthenticationConfiguration configuration) throws Exception {
         return configuration.getAuthenticationManager();
+    }
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .addSecurityItem(new SecurityRequirement()
+                        .addList("bearerAuth"))
+                .components(
+                        new Components()
+                                .addSecuritySchemes("bearerAuth",
+                                        new SecurityScheme()
+                                                .type(SecurityScheme.Type.HTTP)
+                                                .scheme("bearer")
+                                                .bearerFormat("JWT")
+                                )
+                )
+                .info(new Info()
+                        .title("Task list API")
+                        .description("Demo Spring Boot application")
+                        .version("1.0")
+                );
     }
 
 
--- src/main/java/com/dom/tasks/web/controller/AuthController.java
+++ src/main/java/com/dom/tasks/web/controller/AuthController.java
@@ -8,6 +8,7 @@ import com.dom.tasks.web.dto.auth.JwtResponse;
 import com.dom.tasks.web.dto.user.UserDto;
 import com.dom.tasks.web.dto.validation.OnCreate;
 import com.dom.tasks.web.mappers.UserMapper;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,9 +17,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("api/v1/auth")
+@RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
 @Validated
+@Tag(name = "Auth Controller", description = "Auth API")
 public class AuthController {
 
     private final AuthService authService;
@@ -27,18 +29,21 @@ public class AuthController {
     private final UserMapper userMapper;
 
     @PostMapping("/login")
-    public JwtResponse login(@Validated @RequestBody JwtRequest request) {
-        return authService.login(request);
+    public JwtResponse login(@Validated
+                             @RequestBody final JwtRequest loginRequest) {
+        return authService.login(loginRequest);
     }
 
     @PostMapping("/register")
-    public UserDto register(@Validated(OnCreate.class) @RequestBody UserDto userDto) {
-        User createdUser = userService.create(userMapper.toEntity(userDto));
+    public UserDto register(@Validated(OnCreate.class)
+                            @RequestBody final UserDto userDto) {
+        User user = userMapper.toEntity(userDto);
+        User createdUser = userService.create(user);
         return userMapper.userToDto(createdUser);
     }
 
     @PostMapping("/refresh")
-    public JwtResponse refresh(@RequestBody String refreshToken) {
+    public JwtResponse refresh(@RequestBody final String refreshToken) {
         return authService.refresh(refreshToken);
     }
 
